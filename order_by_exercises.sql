a7d78f3ea908763bff08f5e26a1c17e9a79d55ad
Faith Kane <faith.x.kane@gmail.com>
2020-12-14 15:45:41 -0600

    Commit complete where and starting order by

---
+++ order_by_exercises.sql
@@ -0,0 +1,2 @@
+-- 1. Create a new file named order_by_exercises.sql and copy in the contents of your exercise from the previous lesson.
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -21,6 +21,13 @@ FROM employees
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
 AND gender = 'M';
 
+-- We can also write our query like this... (441 observations; it matches.)
+
+SELECT *
+FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+
 -- 5. Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E. (7330 observations)
 
 SELECT *
@@ -45,12 +52,17 @@ SELECT *
 FROM employees 
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
+-- We can also write our query like this... (899 observations; it matches.)
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
 -- How many employees' last names end with E, regardless of whether they start with E? (24_292 observations)
 
 SELECT *
 FROM employees 
 WHERE last_name LIKE '%E';
-
 
 -- 8. Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned. 135_214
 
@@ -88,67 +100,3 @@ WHERE last_name LIKE '%q%';
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
-
--- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
-SELECT *
-FROM employees
-WHERE first_name = 'Irena'
-OR first_name = 'Maya'
-OR first_name = 'Vidya';
-
--- Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
-SELECT *
-FROM employees
-WHERE gender = "M"
-AND (
-first_name = 'Irena'
-OR first_name = 'Maya'
-OR first_name = 'Vidya'
-);
-
--- A and B == B and A
--- A + B == B + A
-SELECT *
-FROM employees
-WHERE (
-first_name = 'Irena'
-OR first_name = 'Maya'
-OR first_name = 'Vidya'
-) 
-AND gender = "M";
-
-
--- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE 'e%'
-OR last_name LIKE '%e';
-
-
--- Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE 'e%'
-AND last_name LIKE '%e';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'e%e';
-
--- Find all employees hired in the 90s and born on Christmas — 362 rows.
-SELECT * 
-FROM employees
-WHERE hire_date LIKE '199%'
-AND birth_date LIKE '%-12-25';
-
--- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
-
--- Find employees hired on Halloween and born in Oct
-SELECT *
-FROM employees
-WHERE hire_date LIKE '%-10-31'
-AND birth_date LIKE '%-10-%';
